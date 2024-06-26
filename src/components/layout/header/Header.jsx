e3af3e6b82b9b88da8184b2103f2209ad0c6acc9
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-06-27 00:29:07 +0500

    addded

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -9,18 +9,29 @@ import home from "../../../assets/icons/home.svg";
 import bar from "../../../assets/icons/bar.svg";
 import { NavLink } from "react-router-dom";
 
-
 const Header = () => {
-  let [fixHeader, setFixHeader] = useState(false);
-  let [showList,setShowList] = useState(false)
+  const [fixHeader, setFixHeader] = useState(false);
+  const [showList, setShowList] = useState(false);
+  console.log(fixHeader);
+
   useEffect(() => {
-    if (window.scrollY === 50) {
-      setFixHeader(true);
-    }
-  }, [window.scrollY]);
+    const handleScroll = () => {
+      if (window.scrollY > 60) {
+        setFixHeader(true);
+      } else {
+        setFixHeader(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <header className="container header">
+    <header className={`container header ${fixHeader ? "fixed" : ""}`}>
       <div className="header__top__nav">
         <div className="header__top__nav__left">
           <select name="lang" id="">
@@ -28,7 +39,7 @@ const Header = () => {
             <option value="UZB">UZB</option>
             <option value="RUS">RUS</option>
           </select>
-          <select name="lang" id="">
+          <select name="currency" id="">
             <option value="USD">USD</option>
             <option value="UZS">UZS</option>
           </select>
@@ -36,46 +47,42 @@ const Header = () => {
         <div className="header__top__nav__right">
           <NavLink to={"/"}>
             <div className="header__top__nav__right__home">
-              <img src={home} alt="" />
-              <h3 >Home</h3>
+              <img src={home} alt="Home" />
+              <h3>Home</h3>
             </div>
           </NavLink>
           <NavLink to={"/admin"}>
-            <img src={account} alt="" />
+            <img src={account} alt="Account" />
             <h3>Account</h3>
           </NavLink>
           <NavLink className="Wishlist" to={"/wishlist"}>
-            <img src={heart} alt="" />
+            <img src={heart} alt="Wishlist" />
             <h3>Wishlist</h3>
           </NavLink>
           <NavLink to={"/cart"}>
-            <img src={cart} alt="" />
+            <img src={cart} alt="Cart" />
             <h3>Cart</h3>
           </NavLink>
           <a className="items">Items</a>
-          <NavLink>
+          <NavLink to={"/explore"}>
             <p>$0.00</p>
-            <img src={search} alt="" />
+            <img src={search} alt="Explore" />
             <h3>Explore</h3>
           </NavLink>
         </div>
       </div>
       <div
-        className={
-          fixHeader
-            ? "header__bottom__nav"
-            : "header__bottom__nav fixed__header__bottom__nav"
-        }
+        className={`header__bottom__nav ${
+          fixHeader ? "fixed__header__bottom__nav" : ""
+        }`}
       >
         <NavLink to={"/"} className="header__bottom__nav__logo">
-          <img src={logo} alt="" />
+          <img src={logo} alt="Logo" />
         </NavLink>
         <ul
-          className={
-            showList
-              ? "header__bottom__nav__list show__list"
-              : "header__bottom__nav__list"
-          }
+          className={`header__bottom__nav__list ${
+            showList ? "show__list" : ""
+          }`}
         >
           <NavLink to={"/"}>HOME</NavLink>
           <NavLink to={"/bags"}>BAGS</NavLink>
@@ -83,17 +90,15 @@ const Header = () => {
           <NavLink to={"/belt"}>BELT</NavLink>
           <NavLink to={"/contact"}>CONTACT</NavLink>
         </ul>
-        {showList ? (
+        {showList && (
           <div onClick={() => setShowList(false)} className="overlay"></div>
-        ) : (
-          <></>
         )}
         <img
           onClick={() => setShowList(true)}
           className="bar"
           width={20}
           src={bar}
-          alt=""
+          alt="Menu"
         />
       </div>
     </header>
