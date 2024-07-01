4ed5d01de52dc67ef11e15975abb86675c23c0b7
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-07-01 08:29:53 +0500

    added

--- src/context/slices/authSlice/index.js
+++ src/context/slices/authSlice/index.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  token: null,
+  token: localStorage.getItem("x-auth-token") || null,
 };
 
 const authSlice = createSlice({
